fb8d253184136d574cd23a13c05dd14825070327
morenamt <onalepeloo@live.com>
2022-07-22 18:43:38 +0200

    refactored the index.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,6 @@
+import App from './App';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
 import { ChakraProvider } from "@chakra-ui/react";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
