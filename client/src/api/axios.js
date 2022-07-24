def0fda527efff51ff0e48558651cdb88fd67d7b
morenamt <onalepeloo@live.com>
2022-07-24 15:25:00 +0200

    updated the client api end point to use the live api deployed to heroku

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 //set the url for the entire application
-const BASE_URL = 'http://localhost:8000';
+const BASE_URL = 'https://micpa.herokuapp.com/';
 
 export default axios.create({
     baseURL: BASE_URL
